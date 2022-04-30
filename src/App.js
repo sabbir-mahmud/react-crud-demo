7b4fa61531578b804aacae50593ee58cf89ae118
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-04-30 20:30:24 +0600

    log in page design added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import LogIn from './Components/Auth/LogIn/LogIn';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
 
     </div>
   );
